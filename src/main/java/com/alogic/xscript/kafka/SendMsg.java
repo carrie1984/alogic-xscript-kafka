6727ac46605800817051d2c5d8097e1e5846357d
cuijialing <cuijialing@hust.edu.cn>
2017-07-08 14:39:47 +0800

    新增功能支持

--- src/main/java/com/alogic/xscript/kafka/SendMsg.java
+++ src/main/java/com/alogic/xscript/kafka/SendMsg.java
@@ -17,6 +17,10 @@ import com.google.gson.Gson;
 public class SendMsg extends AbstractLogiclet{
 	
 	protected String acctId = "";
+	protected String servId = "";
+	protected String billingCycle = "";
+	protected String amount = "";
+	
 	protected String event = "";
 	protected String topic = "";
 	protected String key = "";
@@ -31,13 +35,18 @@ public class SendMsg extends AbstractLogiclet{
 		super.configure(p);
 
 		acctId = PropertiesConstants.getRaw(p, "acctId", acctId);
+		servId = PropertiesConstants.getRaw(p, "servId", servId);
+		billingCycle = PropertiesConstants.getRaw(p, "billingCycle", billingCycle);
+		amount = PropertiesConstants.getRaw(p, "amount", amount);
+		
+		
 		event = PropertiesConstants.getRaw(p, "event", event);
 		topic = PropertiesConstants.getRaw(p, "topic", topic);
 		key = PropertiesConstants.getRaw(p, "key", key);
 		bootstrapServers = PropertiesConstants.getRaw(p, "bootstrapServers", bootstrapServers);
 		}
 	
-	public static void sendMsg(String acctid,String event,String bootstrapservers,String topic,String key)
+	public static void sendMsgChange(String acctid,String event,String bootstrapservers,String topic,String key)
 	{
 		java.util.Properties props = new java.util.Properties();
 		props.put("bootstrap.servers", bootstrapservers);
@@ -61,7 +70,31 @@ public class SendMsg extends AbstractLogiclet{
 
 		producer.close();
 	}
+	public static void sendMsgRbill(String servid,String billcycle,String amount,String event,String bootstrapservers,String topic,String key)
+	{
+		java.util.Properties props = new java.util.Properties();
+		props.put("bootstrap.servers", bootstrapservers);
+		props.put("key.serializer",keySerializer);
+		props.put("value.serializer", valueSerializer);
+		
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+		Map<String, Object> msg = new HashMap<>();
+		msg.put("id", key);
+		msg.put("event", event);
+		Map<String, String> parameters = new HashMap<>();
+		parameters.put("servId", servid);
+		parameters.put("billingCycle", billcycle);
+		parameters.put("amount", amount);
+		msg.put("parameters", parameters);
+		Gson msgGson = new Gson();
+		String msgjson = msgGson.toJson(msg);
+		System.err.println(msgjson);
+		ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic,key,msgjson);
+		
+		producer.send(record);
 
+		producer.close();
+	}
 	@Override
 	protected void onExecute(XsObject root, XsObject current, LogicletContext ctx,
 			ExecuteWatcher watcher){
@@ -69,9 +102,21 @@ public class SendMsg extends AbstractLogiclet{
 		String eventData = ctx.transform(event);
 		String topicData = ctx.transform(topic);
 		String keyData = ctx.transform(key);
+		String servIdData = ctx.transform(servId);
+		String billingCycleData = ctx.transform(billingCycle);
+		String amountData = ctx.transform(amount);
 		String bootstrapServersData = ctx.transform(bootstrapServers);
 		
-		sendMsg(acctIdData, eventData, bootstrapServersData, topicData,keyData);
+		if(eventData.equals("notify.rbill"))//实时费用
+		{
+			sendMsgRbill(servIdData, billingCycleData, amountData, eventData, bootstrapServersData, topicData, keyData);
+		}
+		else 
+		{
+			sendMsgChange(acctIdData, eventData, bootstrapServersData, topicData,keyData);
+		}
+		
+		
 		
 		
 	}
